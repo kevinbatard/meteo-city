f3e0b2607e13329298012b32229afb5f6f6b6b7d
BATARD Kevin (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC BAYONNE) <9503305e@commun.ad.sncf.fr>
2022-11-22 17:01:34 +0100

    feat utilisation de la premiere api pour avoir longitude et latitude et pouvoir utiliser lapi meteo

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const city = document.getElementById('inputCity');
 const btn = document.getElementById('searchBtn');
-
-
-
+const form = document.getElementById('formu');
 
 function getCoord (ville) {
 fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${ville},FR&limit=1&appid=7ebd51d4b0d2f31095d48bb3d6a3a883`)
 .then((response) => response.json())
-.then((data) => console.log(data))
+.then((data) => meteo(data[0].lat, data[0].lon))
+
 };
 
 btn.addEventListener('click', () => {
@@ -15,10 +14,14 @@ btn.addEventListener('click', () => {
     return getCoord(cityName);
 });
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let cityName = city.value;
+    return getCoord(cityName);
+});
 
-
-/* 
+function meteo (lat, lon) {
 fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7ebd51d4b0d2f31095d48bb3d6a3a883`)
 .then((reponse) => reponse.json())
-.then((data) => console.log(data)) */
-
+.then((data) => console.log(data))
+};
