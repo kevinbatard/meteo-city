4b9b7f5e5200dcd2e4f6a7d53c94df651bf7f9d2
BATARD Kevin (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC BAYONNE) <9503305e@commun.ad.sncf.fr>
2022-11-23 11:32:27 +0100

    feat récupération des données du deuxième API(météo), affichage icon et ville dans l'html via js

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const city = document.getElementById('inputCity');
 const btn = document.getElementById('searchBtn');
 const form = document.getElementById('formu');
 
-function getCoord (ville) {
-fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${ville},FR&limit=1&appid=7ebd51d4b0d2f31095d48bb3d6a3a883`)
+
+
+function getCoord (userVille) {
+fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${userVille},FR&limit=1&appid=7ebd51d4b0d2f31095d48bb3d6a3a883`)
 .then((response) => response.json())
 .then((data) => meteo(data[0].lat, data[0].lon))
+displayVille (userVille);
 
 };
 
@@ -21,7 +24,26 @@ form.addEventListener('submit', (event) => {
 });
 
 function meteo (lat, lon) {
-fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7ebd51d4b0d2f31095d48bb3d6a3a883`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7ebd51d4b0d2f31095d48bb3d6a3a883&units=metric`)
 .then((reponse) => reponse.json())
-.then((data) => console.log(data))
+.then((data) => displayData (data)) }
+
+
+function displayData(data) {
+document.getElementById('icone').setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+console.log(data.main.temp)
+console.log(data.main.feels_like)
+console.log(data.main.temp_min)
+console.log(data.main.temp_max)
+console.log(data.wind.deg)
+console.log(data.wind.speed)
+console.log(data.main.humidity)
 };
+
+function displayVille (userVille) {
+    const userCity = document.getElementById('ville');
+    const random = document.createElement('p');
+    userCity.appendChild(random);
+    userCity.textContent= userVille;
+}
+
