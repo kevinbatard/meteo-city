a7f443b927045f1a1f74a7de7af77c6ad16f0745
BATARD Kevin (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC BAYONNE) <9503305e@commun.ad.sncf.fr>
2022-11-24 10:15:55 +0100

    changement d'api, utilisation d'une seule. Ajout du Css bootstrap

--- script.js
+++ script.js
@@ -5,39 +5,42 @@ const form = document.getElementById('formu');
 
 
 function getCoord (userVille) {
-fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${userVille},FR&limit=1&appid=7ebd51d4b0d2f31095d48bb3d6a3a883`)
+fetch(`https://geo.api.gouv.fr/communes?nom=${userVille}&fields=departement&boost=population&limit=5`)
 .then((response) => response.json())
-.then((data) => meteo(data[0].lat, data[0].lon))
+.then((data) => meteo(userVille));
 displayVille (userVille);
+//meteo(userVille))
 
 };
 
 btn.addEventListener('click', () => {
     let cityName = city.value;
+    popIcon ()
     return getCoord(cityName);
 });
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     let cityName = city.value;
+    popIcon ()
     return getCoord(cityName);
 });
 
-function meteo (lat, lon) {
-fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7ebd51d4b0d2f31095d48bb3d6a3a883&units=metric`)
+function meteo (userVille) {
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userVille}&appid=7ebd51d4b0d2f31095d48bb3d6a3a883&units=metric`)
 .then((reponse) => reponse.json())
 .then((data) => displayData (data)) }
 
 
 function displayData(data) {
 document.getElementById('icone').setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
-console.log(data.main.temp)
-console.log(data.main.feels_like)
-console.log(data.main.temp_min)
-console.log(data.main.temp_max)
-console.log(data.wind.deg)
-console.log(data.wind.speed)
-console.log(data.main.humidity)
+document.getElementById('affiTemp').textContent= Math.floor(data.main.temp + 0.5)
+document.getElementById('affiFeel').textContent= Math.floor(data.main.feels_like + 0.5);
+document.getElementById('affiTempMin').textContent= Math.floor(data.main.temp_min + 0.5);
+document.getElementById('affiTempMax').textContent= Math.floor(data.main.temp_max + 0.5);
+document.getElementById('affiSens').textContent=Math.floor(data.wind.deg + 0.5);
+document.getElementById('affiVit').textContent= Math.floor(data.wind.speed + 0.5);
+document.getElementById('humi').textContent= Math.floor(data.main.humidity + 0.5);
 };
 
 function displayVille (userVille) {
@@ -47,3 +50,17 @@ function displayVille (userVille) {
     userCity.textContent= userVille;
 }
 
+function popIcon () {
+    document.getElementById('ico1').classList.remove('invisible');
+    document.getElementById('ico2').classList.remove('invisible');
+    document.getElementById('ico3').classList.remove('invisible');
+    document.getElementById('ico4').classList.remove('invisible');
+    document.getElementById('ico5').classList.remove('invisible');
+    document.getElementById('hideText').classList.remove('invisible');
+    document.getElementById('hideText2').classList.remove('invisible');
+    document.getElementById('hideText3').classList.remove('invisible');
+    document.getElementById('hideText4').classList.remove('invisible');
+    document.getElementById('hideText5').classList.remove('invisible');
+    document.getElementById('hideText6').classList.remove('invisible');
+    document.getElementById('hideText7').classList.remove('invisible');
+}
